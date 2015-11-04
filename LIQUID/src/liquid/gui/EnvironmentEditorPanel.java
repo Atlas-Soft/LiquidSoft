5979d74a34656e3d48c242189ff9a3e75f142423
AtlasSoft-rz4 <rz4@hood.edu>
2015-11-03 23:39:54 -0500

    Rafael Zamora - 11/3/2015

--- LIQUID/src/liquid/gui/EnvironmentEditorPanel.java
+++ LIQUID/src/liquid/gui/EnvironmentEditorPanel.java
@@ -4,17 +4,28 @@ import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import liquid.core.LiquidApplication;
 
 public class EnvironmentEditorPanel extends Panel{
-
+	
+	private static final long serialVersionUID = 1L;
+	
 	private Choice select;
+	private Panel enviroDim;
+	private Panel objects;
+	private Panel sensors;
 	
 	public EnvironmentEditorPanel() {
 		super();
 		initComponents();
 		setLayout(null);
 		setBackground(Color.gray);
-		setBounds(25,180,250,270);
+		setBounds(25,175,250,280);
 		setVisible(true);
 	}
 	
@@ -23,12 +34,28 @@ public class EnvironmentEditorPanel extends Panel{
 		setFont(font);
 		
 		select = new Choice();
-		select.setBounds(5, 5, 240, 30);
+		select.setBounds(5, 5, 240, 25);
 		select.add("Environment Dimension");
 		select.add("Objects");
 		select.add("Sensors");
-		select.setVisible(true);
+		select.addItemListener(new ItemListener(){
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				if(arg0.getItem().toString() == "Environment Dimension"){
+					enviroDim.setVisible(true);
+				}else{
+					enviroDim.setVisible(false);
+				}
+				
+			}
+        });
 		add(select);
+		
+		enviroDim = new Panel();
+		enviroDim.setBounds(0, 35, 250, 245);
+		enviroDim.setBackground(Color.white);
+		add(enviroDim);
+		
 	}
 
 }
--- LIQUID/src/liquid/gui/LiquidMenuBar.java
+++ LIQUID/src/liquid/gui/LiquidMenuBar.java
@@ -16,6 +16,13 @@ public class LiquidMenuBar extends MenuBar{
 
 	private static final long serialVersionUID = 1L;
 	
+	private MenuItem New;
+	private MenuItem load;
+	private MenuItem exit;
+	private MenuItem undo;
+	private MenuItem redo;
+	private MenuItem about;
+	
 	private String logfile;
 
 	public LiquidMenuBar(){
@@ -28,42 +35,48 @@ public class LiquidMenuBar extends MenuBar{
 		setFont(font);
 		
 		Menu m = new Menu("File");
-		MenuItem mi = new MenuItem("New");
-		m.add(mi);
-		mi = new MenuItem("Load...");
-		mi.addActionListener(new ActionListener(){
+		
+		New = new MenuItem("New");
+		m.add(New);
+		
+		load = new MenuItem("Load...");
+		load.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent actionEvent) {
-				 JFileChooser  fileDialog = new JFileChooser("../logs");
-				 fileDialog.setDialogTitle("Load Log File");
-				 fileDialog.setFileFilter(new FileNameExtensionFilter("Log File", "log"));
-				 int returnVal = fileDialog.showOpenDialog(LiquidApplication.getGUI().getLiquidFrame());
-				 if (returnVal == JFileChooser.APPROVE_OPTION) {
-		               logfile = fileDialog.getSelectedFile().getName();
-		               LiquidApplication.getGUI().send(LiquidApplication.getLogger(), LiquidGUI.REQUEST_LOADLOG);
-		            }
+				JFileChooser  fileDialog = new JFileChooser("../logs");
+				fileDialog.setDialogTitle("Load Log File");
+				fileDialog.setFileFilter(new FileNameExtensionFilter("Log File", "log"));
+				int returnVal = fileDialog.showOpenDialog(LiquidApplication.getGUI().getLiquidFrame());
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+		              logfile = fileDialog.getSelectedFile().getName();
+		              LiquidApplication.getGUI().send(LiquidApplication.getLogger(), LiquidGUI.REQUEST_LOADLOG);
+		        }
 			}
         });
-		m.add(mi);
-		mi = new MenuItem("Exit");
-		mi.addActionListener(new ActionListener(){
+		m.add(load);
+		
+		exit = new MenuItem("Exit");
+		exit.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent actionEvent) {
-				 LiquidApplication.getGUI().getLiquidFrame().dispose();
-				 System.exit(0);
+				LiquidApplication.getGUI().getLiquidFrame().dispose();
+				System.exit(0);
 			}
         });
-		m.add(mi);
+		m.add(exit);
 		add(m);
 		
 		m = new Menu("Edit");
-		mi = new MenuItem("Undo");
-		m.add(mi);
-		mi = new MenuItem("Redo");
-		m.add(mi);
+		
+		undo = new MenuItem("Undo");
+		m.add(undo);
+		
+		redo = new MenuItem("Redo");
+		m.add(redo);
 		add(m);
 		
 		m = new Menu("Help");
-		mi = new MenuItem("About");
-		m.add(mi);
+		
+		about = new MenuItem("About");
+		m.add(about);
 		add(m);
 	}
 	
