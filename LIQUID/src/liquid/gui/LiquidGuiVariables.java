d05786cafa7bfbf7ce172583973e83289de6619c
AtlasSoft-rz4 <rz4@hood.edu>
2015-11-05 11:44:37 -0500

    Rafael Zamora - 11/5/2015

--- LIQUID/src/liquid/gui/LiquidGuiVariables.java
+++ LIQUID/src/liquid/gui/LiquidGuiVariables.java
@@ -1,12 +1,16 @@
 package liquid.gui;
 
-public class LiquidGuiVariables {
+import java.util.ArrayList;
+
+public class LiquidGUIVariables {
 	
 	String liquid;
 	float temperature;
 	float viscosity;
 	float runtime;
-	int enviroLength = 500;
-	int enviroWidth = 365;
+	int enviroLength = 410;
+	int enviroWidth = 355;
+	ArrayList<String> obstacles = new ArrayList<String>();
+	
 	
 }
