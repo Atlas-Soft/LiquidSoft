39f9fb2f651e94bf75346cd5ea38eb9ba8b1d4b9
William Steele <billionthb@yahoo.com>
2015-11-20 16:30:56 -0500

    Started implementation of flowmeter
    I have begun developing the Flowmeter class, but realized that the
    equals() function of Vec2 could not produce enough values to get
    frequent readout from flowmeter

--- LIQUID/src/liquid/engine/Flowmeter.java
+++ LIQUID/src/liquid/engine/Flowmeter.java
@@ -3,10 +3,39 @@
  */
 package liquid.engine;
 
+import java.util.ArrayList;
+
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+
 /**
  * @author Rafael Zamora
  *
  */
 public class Flowmeter {
-
+	private World myWorld;
+	private Vec2 myLoc;
+	
+	public Flowmeter(World imHere, Vec2 loc){
+		myWorld = imHere;
+		myLoc = loc;
+	}
+	
+	public Vec2 pollVelocity(){
+		Vec2[] pos = myWorld.getParticlePositionBuffer();
+		ArrayList<Vec2> vel = new ArrayList<Vec2>();
+		for (int i = 0; i < pos.length; i++){
+			//implement almostEqual() to get values from near flowmeter
+		}
+	}
+	
+	  /**
+	   * Determines if two Vec2 objects are within the bounds of each other
+	   * @param a the first Vec2 object
+	   * @param b the second Vec2 object
+	   * @param bounds Object containing x and y radius that b must be in relative to a
+	   */
+	  public final static boolean almostEqual(Vec2 a, Vec2 b, Vec2 bounds){
+		  return false;
+	  }
 }
